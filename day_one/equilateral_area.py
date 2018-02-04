645e49fe8c73780b92bbed19981071a8fbbabcdb
amoghng7 <amoghsk279@gmail.com>
2018-02-04 15:40:09 +0530

    added usd to inr

--- day_one/equilateral_area.py
+++ day_one/equilateral_area.py
@@ -1,4 +1,4 @@
 a = int(input("Enter the length of the sides: "))
 
-area = ((3**0.5)/4) * a**2
-print("The area is {}".format(area))
+area = ((3 ** 0.5) / 4) * a ** 2
+print("The area is {} ".format(area))
